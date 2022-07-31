173c658fb00dfd9268a397c551f2cd05818ce6d2
TAI Zeming <tai.zeming@pithos.net>
2022-07-31 16:35:46 +0800

    Clear strongly typed content

--- examples/print_all.rs
+++
@@ -1,6 +0,0 @@
-use aoe2_probe::scenorio::Scenorio;
-fn main() {
-    let scenorio = Scenorio::from_file("./resources/chapter_1.aoe2scenario");
-    let versio = scenorio.versio();
-    println!("{:?}", versio)
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,1 @@
-pub mod data;
-pub mod generator;
 pub mod scenorio;
-pub mod serializer;
-pub mod versio;
