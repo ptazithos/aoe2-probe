50b4b0b3a78188ab13e4bed89025efc0c6f1745c
TAI Zeming <tai.zeming@pithos.net>
2022-08-02 18:14:33 +0800

    Don't need manually into when push back and update

--- src/prebuilt/ver1_46/cinematics.rs
+++ src/prebuilt/ver1_46/cinematics.rs
@@ -8,9 +8,9 @@ pub struct Cinematics {}
 impl Cinematics {
     pub fn template() -> Token {
         let mut root = LinkedHashMap::with_capacity(3);
-        root.push_back("ascii_pregame", DynString::new(0 as u16, "").into());
-        root.push_back("ascii_victory", DynString::new(0 as u16, "").into());
-        root.push_back("ascii_loss", DynString::new(0 as u16, "").into());
+        root.push_back("ascii_pregame", DynString::new(0 as u16, ""));
+        root.push_back("ascii_victory", DynString::new(0 as u16, ""));
+        root.push_back("ascii_loss", DynString::new(0 as u16, ""));
 
         root.into()
     }
