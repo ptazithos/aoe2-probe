874d3eaa4e818ad53ae1de67ac81afcac4085985
TAI Zeming <tai.zeming@pithos.net>
2022-08-03 19:17:50 +0800

    Export a scenorio to a file

--- src/scenorio.rs
+++ src/scenorio.rs
@@ -1,4 +1,4 @@
-use miniz_oxide::inflate::decompress_to_vec;
+use miniz_oxide::{deflate::compress_to_vec, inflate::decompress_to_vec};
 
 use crate::{
     io::Source,
@@ -6,8 +6,8 @@ use crate::{
     prebuilt::ver1_46,
 };
 use std::{
-    fs::{self, File},
-    io::Read,
+    fs::{self, File, OpenOptions},
+    io::{Read, Write},
 };
 
 pub struct Scenorio {
@@ -21,10 +21,10 @@ impl Scenorio {
         let mut buffer = vec![0; metadata.len() as usize];
         file.read(&mut buffer).expect("buffer overflow");
 
-        Self::from_vec(buffer)
+        Self::from_le_vec(buffer)
     }
 
-    pub fn from_vec(buffer: Vec<u8>) -> Self {
+    pub fn from_le_vec(buffer: Vec<u8>) -> Self {
         let version = Self::get_scenorio_version(&buffer);
         let mut source = Source::new(buffer);
         match version.as_str() {
@@ -48,6 +48,39 @@ impl Scenorio {
             }
             _ => panic!("Unsupport version!"),
         }
+    }
+
+    pub fn to_le_vec(self) -> Vec<u8> {
+        self.versio.to_le_vec()
+    }
+
+    pub fn to_le_export_vec(self) -> Vec<u8> {
+        let file_header = &self.versio.try_map()["file_header"];
+        let mut content = file_header.to_le_vec();
+        let header_size = content.len();
+
+        let uncompressed = &self.versio.to_le_vec()[header_size..];
+        let mut compressed = compress_to_vec(uncompressed, 6);
+
+        content.append(&mut compressed);
+
+        content
+    }
+
+    pub fn to_file(self, file_path: &str) {
+        let buffer = self.to_le_export_vec();
+
+        let path = std::path::Path::new(file_path);
+        let prefix = path.parent().unwrap();
+        std::fs::create_dir_all(prefix).unwrap();
+
+        let mut file = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .open(file_path)
+            .unwrap();
+
+        file.write_all(&buffer).unwrap();
     }
 
     fn get_scenorio_version(buffer: &Vec<u8>) -> String {
