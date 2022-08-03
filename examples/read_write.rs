2bb990e4e4dd18f1324ac64ee6b9d3f446564e04
TAI Zeming <tai.zeming@pithos.net>
2022-08-03 19:27:22 +0800

    Update examples

--- examples/read_write.rs
+++ examples/read_write.rs
@@ -1,7 +1,7 @@
 use aoe2_probe::scenorio::Scenorio;
 
 fn main() {
-    let scenorio = Scenorio::from_file("./resources/temp.aoe2scenario");
-    println!("{:?}", scenorio.versio);
+    let scenorio = Scenorio::from_file("./resources/chapter_1.aoe2scenario");
+    println!("{:?}", &scenorio.versio);
     scenorio.to_file("./resources/temp.aoe2scenario");
 }
