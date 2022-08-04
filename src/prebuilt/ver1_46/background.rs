e3e7b613ff8708da0bae0c37728a217b94ab924e
TAI Zeming <tai.zeming@pithos.net>
2022-08-04 13:32:01 +0800

    Fix: skip bitmap data if background doesn't exist

--- src/prebuilt/ver1_46/background.rs
+++ src/prebuilt/ver1_46/background.rs
@@ -16,9 +16,9 @@ impl Background {
         root.push_back("bitmap_height", 0_u32);
         root.push_back("picture_orientation", 0_i16);
 
-        root.push_back("bitmap_info", BitmapInfo::template());
+        root.push_back("bitmap_info", vec![BitmapInfo::template()]);
         root.patchs.insert(
-            "image".to_string(),
+            "bitmap_info".to_string(),
             NumericPatch {
                 source: vec!["bitmap_width".to_string(), "bitmap_height".to_string()],
                 dep_type: DepType::Exist,
