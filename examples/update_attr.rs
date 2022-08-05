4ee688e4c34cfff638925edcd58550fa267d6c7f
TAI Zeming <tai.zeming@pithos.net>
2022-08-05 16:49:49 +0800

    Get value by path string

--- examples/update_attr.rs
+++ examples/update_attr.rs
@@ -3,21 +3,13 @@ use aoe2_probe::Scenorio;
 fn main() {
     //This example scenario file contains several texts encoded by utf8-cn, enable this locate if you want to see more details.
     let mut scenorio = Scenorio::from_file("./resources/chapter_1.aoe2scenario");
-    let file_header = scenorio.versio.try_mut_map()["file_header"].try_mut_map();
-    //Print the file header.
-    println!(
-        "Original file header with author's Chinese id: \n {:?} \n",
-        &file_header
-    );
 
-    let creator = file_header["creator_name"].try_mut_str32();
+    let file_header = scenorio.versio.get_by_path("/file_header");
+    println!("THe original file header: {:?}", file_header);
 
-    //Update the creator name.
-    creator.set_content("Arian");
+    let author = scenorio.versio.get_by_path_mut("/file_header/creator_name");
+    author.try_mut_str32().set_content("Arian");
 
-    //Print the file header.
-    println!(
-        "Updated file header with the author's English id: \n {:?} \n",
-        &file_header
-    );
+    let file_header = scenorio.versio.get_by_path("/file_header");
+    println!("THe original file header: {:?}", file_header);
 }
