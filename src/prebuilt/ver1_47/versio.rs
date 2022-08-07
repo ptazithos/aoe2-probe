f0a5ed88f856c658b79e60f877dfbe7247e5ea05
TAI Zeming <tai.zeming@pithos.net>
2022-08-07 20:02:28 +0800

    Re-export some ver1.46 to ver1.47

--- src/prebuilt/ver1_47/versio.rs
+++ src/prebuilt/ver1_47/versio.rs
@@ -1,4 +1,3 @@
-use crate::prebuilt::ver1_46;
 use crate::prebuilt::ver1_47;
 use crate::{parse::Token, utils::LinkedHashMap};
 
@@ -8,19 +7,19 @@ pub struct Versio {}
 impl Versio {
     pub fn template() -> Token {
         let mut root = LinkedHashMap::new();
-        root.push_back("file_header", ver1_46::FileHeader::template());
-        root.push_back("data_header", ver1_46::DataHeader::template());
-        root.push_back("message", ver1_46::Message::template());
-        root.push_back("cinematics", ver1_46::Cinematics::template());
-        root.push_back("background_image", ver1_46::Background::template());
-        root.push_back("player_data_two", ver1_46::PlayerDataTwo::template());
-        root.push_back("global_victory", ver1_46::GlobalVictory::template());
-        root.push_back("diplomacy", ver1_46::Diplomacy::template());
-        root.push_back("options", ver1_46::Options::template());
+        root.push_back("file_header", ver1_47::FileHeader::template());
+        root.push_back("data_header", ver1_47::DataHeader::template());
+        root.push_back("message", ver1_47::Message::template());
+        root.push_back("cinematics", ver1_47::Cinematics::template());
+        root.push_back("background_image", ver1_47::Background::template());
+        root.push_back("player_data_two", ver1_47::PlayerDataTwo::template());
+        root.push_back("global_victory", ver1_47::GlobalVictory::template());
+        root.push_back("diplomacy", ver1_47::Diplomacy::template());
+        root.push_back("options", ver1_47::Options::template());
         root.push_back("map", ver1_47::Map::template());
-        root.push_back("units", ver1_46::Units::template());
-        root.push_back("triggers", ver1_46::Triggers::template());
-        root.push_back("files", ver1_46::Files::template());
+        root.push_back("units", ver1_47::Units::template());
+        root.push_back("triggers", ver1_47::Triggers::template());
+        root.push_back("files", ver1_47::Files::template());
         root.into()
     }
 }
